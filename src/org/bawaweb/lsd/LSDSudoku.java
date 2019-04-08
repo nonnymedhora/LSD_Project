7908b93c257e3aa25c18e449759db382bd394243
nonnymedhora <nonnymedhora@github.com>
2019-04-08 21:14:23 +0530

    updated brute-force random-shuffle solution

--- src/org/bawaweb/lsd/LSDSudoku.java
+++ src/org/bawaweb/lsd/LSDSudoku.java
@@ -12,6 +12,87 @@ import org.bawaweb.lsd.LSDPlaced.Cell;
 
 /**
  * @author Navroz
+ * A Brute-force Random Sudoku Solver for a 9 * 9 grid
+ * 	==	each row contains values 1 - 9
+ * 	==	each col contains values 1 - 9
+ * 	==	each 3*3 grid contains values 1 - 9
+ * 
+ * 
+ * No Recursive Backtracking;
+ * just brute force random shuffling employed
+ * 
+ * Output
+ * -------------
+ * Starting with LSD[9] having placed values:
+
+	LSD [9] is
+	
+	 0 0 2 0 0 0 5 0 0
+	 0 0 0 0 7 0 0 0 0
+	 0 0 0 0 0 0 0 0 0
+	 0 0 0 0 0 0 0 0 0
+	 0 0 0 0 0 0 0 0 0
+	 0 0 0 0 0 0 0 0 0
+	 0 0 0 0 0 0 0 0 0
+	 0 9 0 0 0 0 0 0 0
+	 0 0 0 0 3 0 0 0 0
+ 
+  	LSD [9] is
+
+		 1 7 2 3 9 8 5 6 4
+		 4 8 3 5 7 6 2 1 9
+		 9 5 6 1 4 2 8 7 3
+		 5 2 7 9 1 3 4 8 6
+		 6 3 9 4 8 5 7 2 1
+		 8 4 1 6 2 7 9 3 5
+		 7 6 4 8 5 1 3 9 2
+		 3 9 8 2 6 4 1 5 7
+		 2 1 5 7 3 9 6 4 8
+		
+		LSD_ALPHA [9] is
+		
+		 A G B C I H E F D
+		 D H C E G F B A I
+		 I E F A D B H G C
+		 E B G I A C D H F
+		 F C I D H E G B A
+		 H D A F B G I C E
+		 G F D H E A C I B
+		 C I H B F D A E G
+		 B A E G C I F D H
+		 
+		 =======================
+		 
+		 LSD [9] is
+
+		 3 7 2 1 6 8 5 9 4
+		 9 1 5 2 7 4 8 3 6
+		 6 8 4 5 9 3 2 7 1
+		 2 3 9 6 4 7 1 5 8
+		 4 5 7 3 8 1 6 2 9
+		 1 6 8 9 2 5 7 4 3
+		 8 4 3 7 5 6 9 1 2
+		 7 9 6 4 1 2 3 8 5
+		 5 2 1 8 3 9 4 6 7
+		
+		LSD_ALPHA [9] is
+		
+		 C G B A F H E I D
+		 I A E B G D H C F
+		 F H D E I C B G A
+		 B C I F D G A E H
+		 D E G C H A F B I
+		 A F H I B E G D C
+		 H D C G E F I A B
+		 G I F D A B C H E
+		 E B A H C I D F G
+		 
+		 
+		 Note-- if more values were placed
+		 time to solve increases exponentially	--	( but the puzzle does get eventually solved)
+		 so not adding all places
+		 for lack of patience	^__^
+
  *
  */
 public class LSDSudoku extends LSDPlaced {
@@ -22,7 +103,6 @@ public class LSDSudoku extends LSDPlaced {
 	
 	@Override
 	public int[][] generateLSD(Map<Integer, List<Cell>> rowMap, Map<Integer, List<Cell>> colMap) {
-		System.out.println("Here");
 		List<Integer> startList = this.getRowList(rowMap, colMap, 0);
 		boolean added0 = false;
 		while (!added0) {
@@ -30,7 +110,6 @@ public class LSDSudoku extends LSDPlaced {
 				startList = this.getRowList(rowMap, colMap, 0);
 			}
 			added0 = true;
-			System.out.println("Can-add-0");
 		}
 
 		this.matrix[0] = toIntArray(startList);
@@ -43,8 +122,7 @@ public class LSDSudoku extends LSDPlaced {
 				list2Add = getRowList(rowMap, colMap, row);
 				while (!canAdd2Matrix(this.matrix, list2Add, rowMap, colMap, row) && count < 1000000000) {
 					list2Add = getRowList(rowMap, colMap, row);
-					count += 1;// System.out.println("count--gld "+count+"
-								// row is "+row);
+					count += 1;
 				}
 				this.matrix[row] = toIntArray(list2Add);
 			}
@@ -55,7 +133,6 @@ public class LSDSudoku extends LSDPlaced {
 			return this.matrix;
 		}
 
-		System.out.println("Here2");
 		return null;
 
 	}
@@ -67,7 +144,7 @@ public class LSDSudoku extends LSDPlaced {
 		boolean check = false;
 		int count = 1;
 		while (!check && count < 1000000000) {
-			System.out.println("count in getRL "+count+" row is "+row);
+//			System.out.println("count in getRL "+count+" row is "+row);
 			rowList = this.shuffle(getBaseStartList(), rnd);
 			check = this.isOk(rowList, rowM, colM, row);
 			count+=1;
@@ -141,25 +218,24 @@ public class LSDSudoku extends LSDPlaced {
 	protected boolean isOk(List<Integer> list, Map<Integer, List<Cell>> rowMap, Map<Integer, List<Cell>> colMap,
 			int rowSel) {
 		if (super.isOk(list, rowMap, colMap, rowSel)) {
-			System.out.println("super1 ok for row [" + rowSel + "]");
 			
 			// check-boxes
 			for (int col = 0; col < 9; col++) {
 				Cell aCell = new Cell(rowSel, col);
 				if (this.getSudokuBoxValues(aCell).contains(list.get(col))) {
 					if (this.matrix[rowSel][col] != 0 && this.matrix[rowSel][col] == list.get(col)) { // isplaced-cell
-						System.out.println(
-								"isplaced [" + rowSel + "," + col + "] and val is " + this.matrix[rowSel][col]);
+//						System.out.println("isplaced [" + rowSel + "," + col + "] and val is " + this.matrix[rowSel][col]);
 						continue;
 					}
-					System.out.println("SudokuBox contains "+list.get(col));
-					System.out.println("matrix[rc] is "+this.matrix[rowSel][col]);
+//					System.out.println("SudokuBox contains "+list.get(col));
+//					System.out.println("matrix[rc] is "+this.matrix[rowSel][col]);
 					return false;
 				}
 			}
+			return true;
 
 		}
-		return true;
+		return false;
 
 	}
 
