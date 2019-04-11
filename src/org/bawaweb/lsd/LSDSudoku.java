1dbb9e7da129f2563725808f7c60c702114fbf5e
nonnymedhora <nonnymedhora@github.com>
2019-04-11 22:40:25 +0530

    Added backtracking

--- src/org/bawaweb/lsd/LSDSudoku.java
+++ src/org/bawaweb/lsd/LSDSudoku.java
@@ -4,6 +4,7 @@
 package org.bawaweb.lsd;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -18,8 +19,10 @@ import org.bawaweb.lsd.LSDPlaced.Cell;
  * 	==	each 3*3 grid contains values 1 - 9
  * 
  * 
- * No Recursive Backtracking;
- * just brute force random shuffling employed
+ * <strike>No Recursive Backtracking;
+ * just brute force random shuffling employed</strike>
+ * 
+ * Implemented Backtracking
  * 
  * Output
  * -------------
@@ -96,7 +99,11 @@ import org.bawaweb.lsd.LSDPlaced.Cell;
  *
  */
 public class LSDSudoku extends LSDPlaced {
-
+	
+	public LSDSudoku(int b, List<Cell> placedCells) {
+		this(b, getRows(placedCells), getCols(placedCells), getVals(placedCells));
+	}
+	
 	public LSDSudoku(int b, List<Integer> rowC, List<Integer> colC, List<Integer> vals) {
 		super(b,rowC,colC,vals);
 	}
@@ -124,7 +131,13 @@ public class LSDSudoku extends LSDPlaced {
 					list2Add = getRowList(rowMap, colMap, row);
 					count += 1;
 				}
-				this.matrix[row] = toIntArray(list2Add);
+				
+				if (list2Add != null) {
+					this.matrix[row] = toIntArray(list2Add);
+				}
+//				System.out.println("list2Add is null -- Call==remove"+row);
+				this.removeRow(row,rowMap);
+				row -= 1;
 			}
 		}
 		if (checkMatrix(this.matrix, rowMap)) {
@@ -137,6 +150,31 @@ public class LSDSudoku extends LSDPlaced {
 
 	}
 	
+	private void removeRow(final int row, final Map<Integer, List<Cell>> rowMap) {
+		if (rowMap.get(row) == null) {
+			for (int col = 0; col < 9; col++) {
+				this.matrix[row][col] = 0;
+			}
+		} else {
+			if (rowMap.get(row).size() > 0) {
+				List<Integer> placedCols = new ArrayList<Integer>();
+				Map<Integer, Integer> placedVals = new HashMap<Integer, Integer>();
+				for (Cell aCell : rowMap.get(row)) {
+					placedCols.add(aCell.getCol());
+					placedVals.put(aCell.getCol(), aCell.getVal());
+				}
+				for (int col = 0; col < 9; col++) {
+					if (!placedCols.contains(col)) {
+						this.matrix[row][col] = 0;
+					} else {
+						this.matrix[row][col] = placedVals.get(col);
+					}
+				}
+			}
+		}
+//		System.out.println("removed row == " + row);
+	}
+
 	@Override
 	protected List<Integer> getRowList(Map<Integer, List<Cell>> rowM, Map<Integer, List<Cell>> colM,int row) {
 		Random rnd = new Random(this.boundary | System.currentTimeMillis());
@@ -205,8 +243,8 @@ public class LSDSudoku extends LSDPlaced {
 			}
 		}
 		if (!this.isOk(aList, rowMap, colMap, rowS)) {
-			System.out.println(" cannot add for row " + rowS);
-			printList(aList);
+//			System.out.println(" cannot add for row " + rowS);
+//			printList(aList);
 			return false;
 		}
 
@@ -300,31 +338,103 @@ public class LSDSudoku extends LSDPlaced {
 		}
 		return boxList;
 	}
+	private static List<Integer> getVals(List<Cell> cells) {
+		if(cells != null ){
+			List<Integer> rows = new ArrayList<Integer>();
+			for(Cell aCell : cells) {
+				rows.add(aCell.getRow());
+			}
+			return rows;
+		}
+		return null;
+	}
+
+	private static List<Integer> getCols(List<Cell> cells) {
+		if(cells != null ){
+			List<Integer> cols = new ArrayList<Integer>();
+			for(Cell aCell : cells) {
+				cols.add(aCell.getCol());
+			}
+			return cols;
+		}
+		return null;
+	}
+
+	private static List<Integer> getRows(List<Cell> cells) {
+		if(cells != null ){
+			List<Integer> vals = new ArrayList<Integer>();
+			for(Cell aCell : cells) {
+				vals.add(aCell.getVal());
+			}
+			return vals;
+		}
+		return null;
+	}
 
 	public static void main(String[] args) {
 		List<Integer> rows = new ArrayList<Integer>();
-		rows.add(0);
-		rows.add(1);
-		rows.add(7);
-		rows.add(0);
-		rows.add(8);
+		rows.add(0);	rows.add(0);	rows.add(0);	rows.add(0);
+		rows.add(1);	rows.add(1);	rows.add(1);	rows.add(1);
+		rows.add(2);	rows.add(2);	rows.add(2);	rows.add(2);	rows.add(2);
+		rows.add(3);	rows.add(3);	rows.add(3);	rows.add(3);
+		rows.add(4);	rows.add(4);	rows.add(4);	rows.add(4);	rows.add(4);
+		rows.add(5);	rows.add(5);	rows.add(5);	rows.add(5);	rows.add(5);
+		rows.add(6);	rows.add(6);	rows.add(6);	rows.add(6);	rows.add(6);
+		rows.add(7);	rows.add(7);	rows.add(7);	rows.add(7);
+		rows.add(8);	rows.add(8);	rows.add(8);	rows.add(8);
 
 		List<Integer> cols = new ArrayList<Integer>();
-		cols.add(2);
-		cols.add(4);
-		cols.add(1);
-		cols.add(6);
-		cols.add(4);
+		cols.add(2);	cols.add(3);	cols.add(4);	cols.add(6);
+		cols.add(0);	cols.add(4);	cols.add(7);	cols.add(8);
+		cols.add(1);	cols.add(2);	cols.add(3);	cols.add(5);	cols.add(7);	
+		cols.add(0);	cols.add(4);	cols.add(5);	cols.add(6);	
+		cols.add(0);	cols.add(1);	cols.add(3);	cols.add(6);	cols.add(8);
+		cols.add(0);	cols.add(2);	cols.add(5);	cols.add(7);	cols.add(8);
+		cols.add(0);	cols.add(3);	cols.add(5);	cols.add(6);	cols.add(7);
+		cols.add(1);	cols.add(2);	cols.add(3);	cols.add(8);
+		cols.add(1);	cols.add(4);	cols.add(6);	cols.add(8);
 
 		List<Integer> vals = new ArrayList<Integer>();
-		vals.add(2);
-		vals.add(7);
-		vals.add(9);
-		vals.add(5);
-		vals.add(3);
+		vals.add(2);	vals.add(9);	vals.add(8);	vals.add(5);
+		vals.add(4);	vals.add(7);	vals.add(1);	vals.add(3);
+		vals.add(3);	vals.add(9);	vals.add(6);	vals.add(4);	vals.add(7);
+		vals.add(2);	vals.add(5);	vals.add(6);	vals.add(4);
+		vals.add(8);	vals.add(4);	vals.add(3);	vals.add(2);	vals.add(1);
+		vals.add(9);	vals.add(7);	vals.add(1);	vals.add(8);	vals.add(6);
+		vals.add(6);	vals.add(7);	vals.add(5);	vals.add(1);	vals.add(3);
+		vals.add(9);	vals.add(1);	vals.add(4);	vals.add(5);
+		vals.add(2);	vals.add(3);	vals.add(6);	vals.add(8);
 
 		LSDSudoku lsdS = new LSDSudoku(9, rows, cols, vals);
+		
+		//v2
+		/*List<Cell> cells = new ArrayList<Cell>();
+		Cell acell = new Cell(0,2,2);
+		cells.add(acell);
+		
+		LSDSudoku lsdS2 = new LSDSudoku(9,cells);*/
+		
+		
 
 	}
 
 }
+
+//sample taken from bigfish
+	/*
+	 *			1		2		3 
+	 * 	
+	 * A	0 0 2	9 8 0	5 0 0
+	 * 		4 0 0	0 7 0	0 1 3
+	 * 		0 3 9 	6 0 4	0 7 0
+	 * 
+	 * B	2 0 0 	0 5 6 	4 0 0
+	 * 		8 4 0	3 0 0	2 0 1
+	 * 		9 0 7	0 0 1	0 8 6
+	 * 
+	 * C	6 0 0	7 0 5	1 3 0
+	 * 		0 9 1	4 0 0	0 0 5
+	 * 		0 2 0	0 3 0	6 0 8
+	 * 
+	 * 
+	 * */
